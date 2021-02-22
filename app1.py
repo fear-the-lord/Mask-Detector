75d69ef100783d7c41a0e8b5107c47a84fb52fcf
SOUVIK GHOSH <35571958+fear-the-lord@users.noreply.github.com>
2021-02-22 20:18:34 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -9,14 +9,14 @@ def function1():
 	os.system("python image_scrap.py")
 
 def function2(): 
-	os.system("python rename.py")
+	os.system("python rename_images.py")
 
 def function3(): 
 	os.system("python gif_image.py")
 
 def function4(): 
 	root.destroy()
-	os.system("python app.py")
+	os.system("python mask_images.py")
 	
 root.title("MASK DETECTION SYSTEM")
 Label(root, text="MASK DETECTION SYSTEM",font=("times new roman",20),fg="black",bg="#c5cae9",height=2).grid(row=2,rowspan=2,columnspan=5,sticky=N+E+W+S,padx=5,pady=10)
